9bc583cf34a94562e956ba04f23cbd374736eba1
BluSpring <bluspringindohq@gmail.com>
2026-06-05 23:17:03 +0800

    Fix BluSpring/Origins-Legacy#32

--- src/main/java/io/github/apace100/apoli/power/factory/action/ItemActions.java
+++ src/main/java/io/github/apace100/apoli/power/factory/action/ItemActions.java
@@ -20,8 +20,8 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.NbtOps;
 import net.minecraft.nbt.TagParser;
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.Tuple;
@@ -68,7 +68,7 @@ public class ItemActions {
                 MinecraftServer server = worldAndStack.getA().getServer();
                 if(server != null) {
                     Identifier id = data.getId("modifier");
-                    LootItemFunction lootFunction = server.registryAccess().get(Registries.ITEM_MODIFIER).orElseThrow().value().getValue(id);
+                    LootItemFunction lootFunction = server.registryAccess().lookupOrThrow(Registries.ITEM_MODIFIER).getValue(id);
                     if (lootFunction == null) {
                         Apoli.LOGGER.info("Unknown item modifier used in `modify` action: " + id);
                         return;
